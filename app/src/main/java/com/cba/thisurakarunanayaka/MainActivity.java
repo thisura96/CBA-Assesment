8fee83bb4d36bbc6cfd816197c71d6783d42f8f7
Thisura96 <thisura@apextech.lk>
2023-07-19 10:17:27 +0530

    set status bar transparent

--- app/src/main/java/com/cba/thisurakarunanayaka/MainActivity.java
+++ app/src/main/java/com/cba/thisurakarunanayaka/MainActivity.java
@@ -2,7 +2,10 @@ package com.cba.thisurakarunanayaka;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+
     }
 }
